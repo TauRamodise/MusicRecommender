806e300c8a543713c81a00af947247db936679cb
Dimpho <DimphoR@bayportfinance.com>
2022-06-06 20:29:26 +0200

    add initial repo structure

---
+++ src/main/java/com/music/musicrec/MusicRecApplication.java
@@ -0,0 +1,13 @@
+package com.music.musicrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/musicrec/MusicRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.musicrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
