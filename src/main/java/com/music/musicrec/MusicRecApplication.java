8f2754dd5ca1921d18048b4d59da5c779aefa373
Dimpho <DimphoR@bayportfinance.com>
2022-06-12 17:26:37 +0200

    disable data source auto configuring as it fails to load the test application that is looking for an in memory db

--- src/main/java/com/music/musicrec/MusicRecApplication.java
+++ src/main/java/com/music/musicrec/MusicRecApplication.java
@@ -2,8 +2,9 @@ package com.music.musicrec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MusicRecApplication {
 
     public static void main(String[] args) {
