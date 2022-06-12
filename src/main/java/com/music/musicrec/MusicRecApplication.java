ad062f36447ecb45d45342967a5d46c326ccdd6b
Dimpho <DimphoR@bayportfinance.com>
2022-06-12 16:16:33 +0200

    add swagger cofnfign

--- src/main/java/com/music/musicrec/MusicRecApplication.java
+++ src/main/java/com/music/musicrec/MusicRecApplication.java
@@ -2,8 +2,16 @@ package com.music.musicrec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 
 @SpringBootApplication
+@EnableSwagger2
+@Configuration
 public class MusicRecApplication {
 
     public static void main(String[] args) {
