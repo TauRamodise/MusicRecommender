4e230b6a647ce98741876f5e9c90014b06b76b6d
Dimpho <DimphoR@bayportfinance.com>
2022-06-13 14:07:23 +0200

    configure-datasource: fix swagger config

--- src/main/java/com/music/musicrec/MusicRecApplication.java
+++ src/main/java/com/music/musicrec/MusicRecApplication.java
@@ -3,13 +3,9 @@ package com.music.musicrec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableSwagger2
 public class MusicRecApplication {
 
