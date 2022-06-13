d6182ab0d305ac3874366d3bd4cd73ea2d0e8588
Dimpho <DimphoR@bayportfinance.com>
2022-06-13 20:19:57 +0200

    datasource-config: move the datasource config to correct position

--- src/main/java/com/music/musicrec/domain/TracksEntity.java
+++ src/main/java/com/music/musicrec/domain/TracksEntity.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
@@ -12,7 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Setter
-public class TracksEntity {
+public class TracksEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
