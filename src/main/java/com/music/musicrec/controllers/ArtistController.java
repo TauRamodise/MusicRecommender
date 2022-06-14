811416802f86613d8295d2a8a946bb1bfbdd8379
riseller <riseller@bbd.co.za>
2022-06-14 13:58:56 +0200

    Attempt 1 of combining controllers need to add Amys and Pieters

--- src/main/java/com/music/musicrec/controllers/ArtistController.java
+++ src/main/java/com/music/musicrec/controllers/ArtistController.java
@@ -2,34 +2,57 @@ package com.music.musicrec.controllers;
 
 import com.music.musicrec.domain.ArtistsEntity;
 import com.music.musicrec.exceptions.MappingException;
-import com.music.musicrec.models.ArtistSearchRequest;
 import com.music.musicrec.models.ArtistSearchResponse;
 import com.music.musicrec.services.ArtistServiceImpl;
 import com.music.musicrec.util.SearchControllerUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.music.musicrec.util.SearchControllerUtil.mapToSearchResponse;
-
+import static com.music.musicrec.util.SearchControllerUtil.mapToSearchResponseExample;
 
 @RestController
-public class SimilarArtistsController {
+public class ArtistController {
+
     private ArtistServiceImpl artistService;
 
     @Autowired
-    public SimilarArtistsController(ArtistServiceImpl artistService) {
+    public ArtistController(ArtistServiceImpl artistService) {
         this.artistService = artistService;
+    }
+
+    @ApiOperation("Get a list of all genres")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful Operation"),
+            @ApiResponse(code = 400, message = "Invalid Request"),
+            @ApiResponse(code = 500, message = "Unknown Error Occurred")
+    })
+    @GetMapping(value = "/all-genres", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getAllGenres() {
+        List<String> allGenres = artistService.getGenres();
+
+        return ResponseEntity.ok(allGenres);
+    }
+
+    @ApiOperation("Search for one Artist")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful Operation"),
+            @ApiResponse(code = 400, message = "Invalid Request"),
+            @ApiResponse(code = 500, message = "Unknown Error Occurred")
+    })
+    @GetMapping(value = "/search-all-artist/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ArtistSearchResponse> getOneArtists(@PathVariable String id) {
+
+        return ResponseEntity.ok(mapToSearchResponseExample());
     }
 
     @ApiOperation("Search for Similar Artists")
@@ -40,7 +63,8 @@ public class SimilarArtistsController {
     })
     @GetMapping(value = "/find-similar-artists/{artistName}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ArtistSearchResponse>> findSimilarArtists(@PathVariable String artistName) throws Exception {
-        try {
+        try
+        {
             List<ArtistsEntity> getAllArtists = artistService.findSimilarArtists(artistName);
             List<ArtistSearchResponse> getAll = getAllArtists.stream().map(SearchControllerUtil::mapToSearchResponse).collect(Collectors.toList());
             return ResponseEntity.ok(getAll);
@@ -48,4 +72,6 @@ public class SimilarArtistsController {
             throw new MappingException("We could not find any similar artists", e.getCause());
         }
     }
+
+
 }
--- src/main/java/com/music/musicrec/controllers/TracksController.java
+++ src/main/java/com/music/musicrec/controllers/TracksController.java
@@ -3,6 +3,7 @@ package com.music.musicrec.controllers;
 import com.music.musicrec.domain.TracksEntity;
 import com.music.musicrec.exceptions.MappingException;
 import com.music.musicrec.models.SongsByMoodRequest;
+import com.music.musicrec.models.TopSongsRequest;
 import com.music.musicrec.models.TrackSearchResponse;
 import com.music.musicrec.services.TracksServiceImpl;
 import com.music.musicrec.util.TrackControllerUtil;
@@ -19,16 +20,13 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @RestController
-
-public class SongsByMoodController {
-
-
+public class TracksController
+{
     private TracksServiceImpl tracksService;
 
     @Autowired
-    public SongsByMoodController(TracksServiceImpl tracksService){this.tracksService=tracksService;}
+    public TracksController(TracksServiceImpl tracksService){this.tracksService=tracksService;}
 
     @ApiOperation("Search for songs based on your mood")
     @ApiResponses(value = {
@@ -43,4 +41,24 @@ public class SongsByMoodController {
         return ResponseEntity.ok(getSongs);
     }
 
+    @ApiOperation("Get Top 50 Songs For A Given Year")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful Operation"),
+            @ApiResponse(code = 400, message = "Invalid Request"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Unknown Error Occurred")
+    })
+    @GetMapping(value = "/get-top-songs", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<TrackSearchResponse>> getTopSongs(@Valid TopSongsRequest topSongsRequest) throws Exception {
+        try {
+            List<TracksEntity> getTopSongs = tracksService.getTopSongs(topSongsRequest.getYear());
+            List<TrackSearchResponse> getAll = getTopSongs.stream().map(TrackControllerUtil::mapToSearchResponse).collect(Collectors.toList());
+            return ResponseEntity.ok(getAll);
+        }
+        catch (MappingException e) {
+            throw new MappingException("No records found for given year", e.getCause());
+        }
+    }
+
 }
+
