4c2b9a241fb9aa2d4d8fe86541d5f1f616305c3f
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-06-14 11:51:41 +0200

    errors implemented

--- src/main/java/com/music/musicrec/controllers/SearchController.java
+++ src/main/java/com/music/musicrec/controllers/SearchController.java
@@ -1,6 +1,7 @@
 package com.music.musicrec.controllers;
 
 import com.music.musicrec.domain.ArtistsEntity;
+import com.music.musicrec.exceptions.MappingException;
 import com.music.musicrec.models.ArtistSearchResponse;
 import com.music.musicrec.services.ArtistServiceImpl;
 import com.music.musicrec.util.SearchControllerUtil;
@@ -62,9 +63,13 @@ public class SearchController {
             @ApiResponse(code = 500, message = "Unknown Error Occurred")
     })
     @GetMapping(value = "/get-artists-by-genre/{genre}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ArtistSearchResponse>> getArtistsByGenre(@PathVariable String genre) {
-        List<ArtistsEntity> getArtistsByGenre = artistService.getArtistsByGenre(genre);
-        List<ArtistSearchResponse> top10 = getArtistsByGenre.stream().map(SearchControllerUtil::mapToSearchResponse).collect(Collectors.toList());
-        return ResponseEntity.ok(top10);
+    public ResponseEntity<List<ArtistSearchResponse>> getArtistsByGenre(@PathVariable String genre) throws MappingException {
+        try {
+            List<ArtistsEntity> getArtistsByGenre = artistService.getArtistsByGenre(genre);
+            List<ArtistSearchResponse> top10 = getArtistsByGenre.stream().map(SearchControllerUtil::mapToSearchResponse).collect(Collectors.toList());
+            return ResponseEntity.ok(top10);
+        } catch (MappingException e) {
+            throw new MappingException("That genre was not found in the database.", e.getCause());
+        }
     }
 }
